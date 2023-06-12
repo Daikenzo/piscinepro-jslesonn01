8280179498e1356b1e08d8ffeced9c6365349c34
Charles BRU(Daikenzo) <charles.bru@lapiscine.pro>
2023-06-12 11:27:26 +0200

    2023-06-12 - uodate display mode

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,35 @@
 // Variable Constante et Globale
-let Number1 = 20;
-let Number2 = 10;
+let _Result = 0;
+let displayConsigne, displayMessage= "";
+
+let Number1 = 23;
+let Number2 = 20;
+let Number3 = 60;
+
 
 // Main Program
-logMessage(`Le résultat de la somme de ${Number1} et de ${Number2} est de:`);
-sum2args(Number1, Number2);
+    //
+    displayConsigne = ``;
+
+
+
+// Display 
+
+logMessage(`Le Résultat du process de ${displayConsigne} est égale à ${_Result}`)
 
 //Old etape conservée
 /*
 
+    //Sum
+_Result = sum2args(Number1, Number2);
+logMessage(`Le résultat de la somme de ${Number1} et de ${Number2} est de: ${_Result}`);
+/* 3 function
+1 - return sum 2 args
+2 - return de 2args (dont 1 lié au result du 1)
+3 log message //
+_Result = sum2args(Number1, Number2);
+_Result = sub2args(_Result, Number3);
+logMessage(`La somme du process de ${Number1} + ${Number2} - ${Number3} est égale à ${_Result}`);
 */
 
 //Function
@@ -24,6 +45,11 @@ function sum2args(arg1 = 0, arg2 = 0){
     // écriture une foncion qui affiche "La somme de .. avec est égale à "
 
     // display
-    const result = `${arg1 + arg2}`;
-    console.log (result);
+    const result = arg1 + arg2;
+    return result; // Retourne la valeur - Equivaut à console.log(Result) mais stock la valeur en dehors
+}
+function sub2args(arg1 = 0, arg2 =0){
+    // subtraction
+    const result = arg1 - arg2;
+    return result;
 }
