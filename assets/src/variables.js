4a5b81ac3729211b36d8b33a30a4a8190e1a875d
Charles BRU(Daikenzo) <charles.bru@lapiscine.pro>
2023-06-14 16:10:21 +0200

    Correct mozaik main-containers background

---
+++ assets/src/variables.js
@@ -0,0 +1,14 @@
+// Variable Glogal Projects
+const _container = document.querySelector('.container')
+
+const colors = [_container.style.bacgroundColor,
+    'white',
+    'black',
+    '#222354', 
+    '#212831', 
+    '#414132', 
+    '#652304',
+    '#2d4055'
+]
+
+
