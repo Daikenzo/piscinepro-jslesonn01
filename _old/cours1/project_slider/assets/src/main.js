3c03b968e6484ac89b5602b64e1a37fe6e0e7fbb
Charles BRU(Daikenzo) <charles.bru@lapiscine.pro>
2023-06-15 17:14:34 +0200

    init slider project css base

---
+++ _old/cours1/project_slider/assets/src/main.js
@@ -0,0 +1,7 @@
+// Const and Var
+
+const slidesData = [
+    { title: "VILLAGE", description: "Lorem ipsum dolor sit amet" },
+    { title: "VIGNE", description: "Consectetur adipiscing elit" },
+    { title: "CHAMPS", description: "Sed do eiusmod tempor incididunt" },
+]
