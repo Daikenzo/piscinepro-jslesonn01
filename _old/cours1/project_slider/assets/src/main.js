4cb8460f591cf48a28277942e1759e051928eefc
Charles BRU(Daikenzo) <charles.bru@lapiscine.pro>
2023-06-16 10:03:41 +0200

    init project json intro(foreach)

--- _old/cours1/project_slider/assets/src/main.js
+++ _old/cours1/project_slider/assets/src/main.js
@@ -5,3 +5,21 @@ const slidesData = [
     { title: "VIGNE", description: "Consectetur adipiscing elit" },
     { title: "CHAMPS", description: "Sed do eiusmod tempor incididunt" },
 ]
+// Btn & slider
+const btns = document.querySelectorAll('.slider-btn');
+const sliderPictureContain = document.querySelector('.slide-container');
+
+//IMG List
+const pictureList = document.querySelectorAll('.slider-img');
+//TXT 
+const sliderTitle = document.querySelector('.slider-title');
+const sliderLegends = document.querySelector('.slider-legend');
+
+
+
+// btn class : slider-btn - left : slider-btn-left right : slider-btn-right
+
+//container img + elemebt slider-img-container
+// pic div container : .slide-container -> list img : .slider-img
+// title img display class : slider-title
+// describe (legend) : slider-legend-container, txt : slider-legend
