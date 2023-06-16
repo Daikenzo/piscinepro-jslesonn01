924dc2cad297299073323d5e53be9499e772d404
Charles BRU(Daikenzo) <charles.bru@lapiscine.pro>
2023-06-16 13:26:35 +0200

    corect exo 4

--- _old/cours2/04_jsondataforeach/src/index.js
+++ _old/cours2/04_jsondataforeach/src/index.js
@@ -30,42 +30,70 @@ console.log(myExampleFunction(myExempleFunctionArg[0], myExempleFunctionArg[1]))
 
 // foreach // ne peut être fait que sur les tableau
 // on ne peut pas faire de break / continue et autre fonction de boucle) 
-const myArray1 = [2,5,6,7];
-myArray1.forEach((el) => {
-    console.log('hello')
-});
+// const myArray1 = [2,5,6,7];
+// myArray1.forEach((el) => {
+//     console.log('hello')
+// });
 
 // display log
 function displayResult(ExoNumber, element){
-    console.log(`Result Exo N°${ExoNumber}: 
-    ${element}`)
+    console.log(`Result Exo N°${ExoNumber} :`)
+    console.log(element);
 }
 
 function RunExo(){
     let ExoResult;
     // a. Logger le premier element du tableau games
-    // displayResult(`0a`, gamesFirstElement);
+    displayResult(`0a`, games[0]);
     // b. Logger l'annee du 3eme element du tableau games
-    // displayResult(`0b`, games[2].year);
+    displayResult(`0b`, games[2].year);
     // c. Logger le titre du dernier element du tableau
-    //displayResult(`0c`, games[games.length-1].title);
+    displayResult(`0c`, games[games.length-1].title);
 
 
 // 1. Ecrire une fonction qui prend en parametre le tableau games et qui retourne un nouveau tableau de tous les noms de jeux (.title)
-    const getAllTitles = (arr) => {
-        let titles = []
-        arr.forEach(game => { // gameTitleList.push(game.title)
-        titles.push(game.title)}
-        )     // accolade inutile si une seule ligne
+const getAllTitles = (arr) => {
+    let titles = []
+    arr.forEach(game => { // gameTitleList.push(game.title)
+    titles.push(game.title)}
+    )     // accolade inutile si une seule ligne
     return titles 
 }
         
     ExoResult = getAllTitles(games);
     displayResult(1, ExoResult);
 // 2. Ecrire une fonction qui prend en parametre une annee et un tableau (games), et qui retourne un nouveau tableau de noms de jeux de l'annee specifiee
+const   getGameNamesYears = (getYear, arr) => {
+    let result = [];
+
+    arr.forEach((el) => {
+        if(getYear === el.year){
+            result.push(el.title)
+        }
+    })
+    return result;
+}
+ExoResult = getGameNamesYears(2021, games);
+displayResult(2, ExoResult);
 
 // 3. Ecrire une fonction qui prend en parametre un tableau (games) retourne un tableau de noms de jeux, a condition que leur note soit superieur ou egal a 8
+const getGameNameByRate = (rating, arr) => {
+    let titles =    []; // variable de récup tableau final
+    
+    //
+    arr.forEach((el) => {
+        if( rating <= el.rate){
+            titles.push(el.title)
+        }
+    })
 
-// 4. Ecrire une fonction qui prend en parametre une console et un tableau (games), et qui retourne un nouveau tableau de tous les jeux disponibles sur cette console
+    return titles; // retourner tableau compléter
+}
 
+ExoResult = getGameNameByRate(7, games);
+displayResult(3, ExoResult);
+// 4. Ecrire une fonction qui prend en parametre une appareil/ console et un tableau (games), et qui retourne un nouveau tableau de tous les jeux disponibles sur cette console
+
+ExoResult;
+displayResult(4, ExoResult);
 }
