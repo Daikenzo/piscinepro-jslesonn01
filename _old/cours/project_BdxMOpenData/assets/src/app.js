ca3589a526a0cf6a310f658258fcd5754c81d4e7
Charles BRU(Daikenzo) <charles.bru@lapiscine.pro>
2023-06-20 10:44:16 +0200

    init project - OpenDataBdx

---
+++ _old/cours/project_BdxMOpenData/assets/src/app.js
@@ -0,0 +1,33 @@
+//Constant & Var
+// Btn
+const refreshBtn = 0
+
+//Dataset
+
+
+//Function
+async function fetchData() {
+    try {
+        const response = await fetch(`https://opendata.bordeaux-metropole.fr/api/records/1.0/search/?dataset=${dataset}`)
+
+        if (!response.ok) {
+            throw new Error(`Erreur: ${response.status}`)
+        }
+
+        const data = await response.json()
+        console.log(data)
+        // btnChange()
+        // displayData(data);
+    }
+    catch (error) {
+        // btnChange()
+        // errorMsg.textContent = `${error}`
+    }
+}
+
+
+// Init
+function init(à{
+
+
+})
